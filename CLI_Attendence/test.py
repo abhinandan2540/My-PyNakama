bcc9e50543bb158ec6df798a6ed58c6ebc757633
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-02-08 22:57:43 +0530

    attendenceTracker

--- CLI_Attendence/test.py
+++ CLI_Attendence/test.py
@@ -1,6 +1,17 @@
 from argparse import ArgumentParser
 import os
 import json
+# for sending the email
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+load_dotenv()
+
+sender_mail = os.getenv("SENDER")
+reciever_mail = os.getenv("RECIEVER")
+app_password = os.getenv("APP_PASSWORD")
+
 
 # reading,writing,saving student credentials
 
@@ -10,6 +21,25 @@ if os.path.exists(data_sheet):
         students = json.load(file)
 else:
     students = []
+
+
+def sendStudentEmails(student_name, student_subject, student_total_leave):
+    msg = MIMEMultipart()
+    msg["From"] = sender_mail
+    msg["To"] = reciever_mail
+    msg["Subject"] = "warning!!! leaving exceeded!!!"
+    msg.attach(MIMEText(
+        f"{student_name} for subject {student_subject} exceeded limit of {student_total_leave} leave"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_mail, app_password)
+        server.sendmail(sender_mail, reciever_mail, msg.as_string())
+        server.quit()
+        print("exceeded mail send sucessfully")
+    except Exception as e:
+        print(f"Error : {e}")
 
 
 def save_data():
@@ -27,6 +57,9 @@ def addStudentCredentials(student_name, student_subject, student_total_leave):
     save_data()
     print("student credentials added...")
 
+    if student_total_leave > 3:
+        sendStudentEmails(student_name, student_subject, student_total_leave)
+
 
 def updateStudentCredentials():
     studentName = input("Enter student name :")
@@ -36,6 +69,12 @@ def updateStudentCredentials():
             student['student_total_leave'] = int(
                 input("student_total_leave : "))
             save_data()
+            print(f"{studentName} credentials updated...")
+
+            if student['student_total_leave'] > 3:
+                sendStudentEmails(
+                    studentName, student['student_subject'], student['student_total_leave'])
+
         else:
             print(f"{studentName} not found")
 
@@ -45,6 +84,9 @@ def viewStudentCredentials():
         for student in students:
             print(
                 f"student_name : {student['student_name']}, student_subjects:{student['student_subject']}, student_total_leave: {student['student_total_leave']}")
+            if (student['student_total_leave'] > 3):
+                sendStudentEmails(
+                    student['student_name'], student['student_subject'], student['student_total_leave'])
     else:
         print("no record found")
 
