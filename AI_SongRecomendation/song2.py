65720058c6bf817cb5a44faa93fee235670e75af
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-03-13 13:47:18 +0530

    example-2

--- AI_SongRecomendation/song2.py
+++ AI_SongRecomendation/song2.py
@@ -3,3 +3,21 @@ from google.genai import types
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+
+def Gemini_song_recomendation(user_input):
+    gemini_instructions = "you are elon musk"
+
+    api = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api)
+    response = client.models.generate_content_stream(
+        model="gemini-2.0-flash",  config=types.GenerateContentConfig(
+            system_instruction=gemini_instructions),
+        contents=[user_input])
+
+    for chunk in response:
+        print(chunk.text, end="")
+
+
+user_input = input("Ask Query:")
+Gemini_song_recomendation(user_input=user_input)
