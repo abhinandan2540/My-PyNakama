4f60a34a14e6979de446df699ef2726362e3fe2d
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2025-03-13 13:58:20 +0530

    example-1-song-recomendation

--- AI_SongRecomendation/song1.py
+++ AI_SongRecomendation/song1.py
@@ -4,14 +4,32 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-api = os.getenv("GEMINI_API_KEY")
+# api = os.getenv("GEMINI_API_KEY")
+# user_input = input("Ask a query: ")
+# sys_instruct = "you are Arijit Singh"
+# client = genai.Client(api_key=api)
+# response = client.models.generate_content_stream(
+#     model="gemini-2.0-flash",  config=types.GenerateContentConfig(
+#         system_instruction=sys_instruct),
+#     contents=[user_input]
+# )
+# for chunk in response:
+#     print(chunk.text, end="")
+
+
+def Gemini_Song_Recomendation(user_query):
+    api = os.getenv("GEMINI_API_KEY")
+    system_instruction = "You are Arijit Singh"
+    client = genai.Client(api_key=api)
+
+    response = client.models.generate_content_stream(
+        model="gemini-2.0-flash",  config=types.GenerateContentConfig(
+            system_instruction=system_instruction),
+        contents=[user_input]
+    )
+    for chunk in response:
+        print(chunk.text, end="")
+
+
 user_input = input("Ask a query: ")
-sys_instruct = "you are a musician, your name is arijit"
-client = genai.Client(api_key=api)
-response = client.models.generate_content_stream(
-    model="gemini-2.0-flash",  config=types.GenerateContentConfig(
-        system_instruction=sys_instruct),
-    contents=[user_input]
-)
-for chunk in response:
-    print(chunk.text, end="")
+Gemini_Song_Recomendation(user_query=user_input)
